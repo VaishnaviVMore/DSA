19a729421000953dee8c0d2787dc7293a019d978
Vaishnavi More <07mvaishnavi@gmail.com>
2025-06-10 09:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (68.15%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -6,12 +6,12 @@ class MyQueue {
     }
     
     public void push(int x) {
-        while(St1.size()!=0){
+        while(!St1.isEmpty()){
             St2.push(St1.peek());
             St1.pop();
         }
         St1.push(x);
-        while(St2.size()!=0){
+        while(!St2.isEmpty()){
             St1.push(St2.peek());
             St2.pop();
         }
